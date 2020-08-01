28968ec606ef2d484cc5c98d4b5c839b0adccbba
하경민 <gaonrudal@gmail.com>
2020-08-01 12:04:43 +0900

    Dockerizing flutter dev env
    - Make docker-compose.yml
    - Set .gitignore
    - Flutter new project
    - But, it occurs errors

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-인공지능 모델
+# 반려견 지능형 배변훈련 서비스 '굿푸피' - Frontend repo (앱, Flutter)
+
+MAINTAINER
+하경민
+gaonrudal@gmail.com
---
+++ gppapp/android/app/src/main/kotlin/com/example/gppapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gppapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

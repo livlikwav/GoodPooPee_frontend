bcee7607be2ca00e9851a43f5514fbd694390a4c
Gyeongmin Ha <gaonrudal@gmail.com>
2020-09-19 03:07:53 +0900

    Refine setting > feedback screen

--- gpp_app/lib/screens/settings/screens/feedback.dart
+++ gpp_app/lib/screens/settings/screens/feedback.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:gpp_app/routes.dart';
+import 'package:gpp_app/constants/assets.dart';
 import 'package:gpp_app/util/size_config.dart';
+import 'package:gpp_app/widgets/custom_text_field.dart';
+import 'package:gpp_app/widgets/default_button.dart';
 
-class SettingFeedbackScreen extends StatelessWidget {
+class SettingFeedbackScreen extends StatefulWidget {
+  @override
+  _SettingFeedbackScreenState createState() => _SettingFeedbackScreenState();
+}
+
+class _SettingFeedbackScreenState extends State<SettingFeedbackScreen> {
+  // Default: On
+  bool isOn = true;
+  // Default : true
+  bool isSliderEnabled = true;
+  // Default: 100%
+  double feedbackRatio = 100.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,47 +41,46 @@ class SettingFeedbackScreen extends StatelessWidget {
           child: Column(
             children: [
               Container(
+                padding: EdgeInsets.all(getBlockSizeHorizontal(5)),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius:
                       BorderRadius.circular(getBlockSizeHorizontal(5)),
                 ),
-                child: ListView(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  padding: EdgeInsets.all(getBlockSizeVertical(3)),
-                  children: <Widget>[
-                    ListTile(
-                      leading: Icon(Icons.portrait),
-                      title: Text('회원 정보 설정'),
-                      onTap: () =>
-                          Navigator.of(context).pushNamed(Routes.report),
+                child: Column(
+                  children: [
+                    _feedbackAvatar(),
+                    Container(
+                      padding: EdgeInsets.all(getBlockSizeHorizontal(3)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          _subtitle('간식 급여 설정(켜기/끄기)'),
+                          _buildSwitch(),
+                        ],
+                      ),
                     ),
-                    ListTile(
-                      leading: Icon(Icons.portrait),
-                      title: Text('반려견 정보 설정'),
-                      onTap: () =>
-                          Navigator.of(context).pushNamed(Routes.report),
+                    SizedBox(height: getBlockSizeVertical(5)),
+                    Container(
+                      padding: EdgeInsets.all(getBlockSizeHorizontal(3)),
+                      decoration: BoxDecoration(
+                        color: isSliderEnabled
+                            ? Colors.transparent
+                            : Colors.grey.withOpacity(0.7),
+                        borderRadius:
+                            BorderRadius.circular(getBlockSizeHorizontal(5)),
+                      ),
+                      child: Column(
+                        children: [
+                          _subtitle('간식 급여 확률 설정'),
+                          _buildSlider(),
+                          _buildIndicator(),
+                        ],
+                      ),
                     ),
-                    Divider(),
-                    ListTile(
-                      leading: Icon(Icons.wifi),
-                      title: Text('푸피캠/푸피스낵바 연결'),
-                      onTap: () =>
-                          Navigator.of(context).pushNamed(Routes.report),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.access_alarm),
-                      title: Text('배변패드 위치 설정'),
-                      onTap: () =>
-                          Navigator.of(context).pushNamed(Routes.report),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.access_alarm),
-                      title: Text('간식 급여 설정'),
-                      onTap: () =>
-                          Navigator.of(context).pushNamed(Routes.report),
-                    ),
+                    SizedBox(
+                        width: getBlockSizeHorizontal(80),
+                        height: getBlockSizeVertical(1)),
                   ],
                 ),
               ),
@@ -75,4 +88,75 @@ class SettingFeedbackScreen extends StatelessWidget {
           )),
     );
   }
+
+  CircleAvatar _feedbackAvatar() {
+    return CircleAvatar(
+      radius: getBlockSizeHorizontal(10),
+      child: Icon(
+        Icons.restaurant,
+        color: Colors.orange,
+        size: getBlockSizeHorizontal(10),
+      ),
+    );
+  }
+
+  Align _subtitle(String text) {
+    return Align(
+      child: Padding(
+          padding: EdgeInsets.all(
+            getBlockSizeHorizontal(2),
+          ),
+          child: Text(text)),
+      alignment: Alignment.centerLeft,
+    );
+  }
+
+  Widget _buildSwitch() {
+    return Switch(
+      value: isOn,
+      onChanged: (bool value) {
+        setState(() {
+          isOn = value;
+          // Set Slider enable/disable
+          isOn ? isSliderEnabled = true : isSliderEnabled = false;
+        });
+      },
+    );
+  }
+
+  Widget _buildSlider() {
+    return Container(
+      child: Slider.adaptive(
+        value: feedbackRatio,
+        min: 0.0,
+        max: 100.0,
+        onChanged: isSliderEnabled
+            ? (newValue) {
+                setState(() {
+                  feedbackRatio = newValue;
+                });
+              }
+            : null,
+        divisions: 4,
+        // Disable in Slider.adaptive
+        // label: '$feedbackRatio',
+      ),
+    );
+  }
+
+  Widget _buildIndicator() {
+    return Row(
+      children: [
+        Expanded(child: Center(child: Text('0%'))),
+        Expanded(child: Center(child: Text('25%'))),
+        Expanded(child: Center(child: Text('50%'))),
+        Expanded(child: Center(child: Text('75%'))),
+        Expanded(child: Center(child: Text('100%'))),
+      ],
+    );
+  }
+
+  // void _userDelete() {
+  //   print('user delete tapped');
+  // }
 }
